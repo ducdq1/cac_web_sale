9a4aa21a860b66f73a4092febec69a25ff0bf64d
ducdq1 <duc.doanquang@gmail.com>
2023-11-26 10:56:31 +0700

    u

--- wp-config.php
+++ wp-config.php
@@ -23,10 +23,10 @@
 define( 'DB_NAME', 'cac' );
 
 /** MySQL database username */
-define( 'DB_USER', 'cac' );
+define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'Leppro90' );
+define( 'DB_PASSWORD', '' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
